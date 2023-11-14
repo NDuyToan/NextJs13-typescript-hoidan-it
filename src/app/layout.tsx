84cb029f3c25e3c44690f96d469beecbe41ebc94
toan_nguyen <ndtoan@enjoyworks.co.kr>
2023-11-14 17:35:22 +0700

    17: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>header ne</header>
+        {children}
+        <footer>footer ne</footer>
+      </body>
     </html>
   );
 }
