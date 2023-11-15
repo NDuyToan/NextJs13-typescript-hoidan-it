566be53613f198b9bc8b447f4ec2a6dd22239707
toan_nguyen <ndtoan@enjoyworks.co.kr>
2023-11-15 09:29:21 +0700

    18: css with nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,25 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+
+import stylesApp from "@/styles/app.module.css";
+import stylesFace from "@/styles/face.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <ul>
-        <li className={styles.item}>
-          <Link href="/facebook">Facebook</Link>
+        <li className={stylesApp.red}>
+          <Link href="/facebook">
+            <p className={stylesFace.red}>Facebook</p>
+          </Link>
         </li>
-        <li className={styles.item}>
+        <li>
           <Link href="/youtube">Youtube</Link>
         </li>
-        <li className={styles.item}>
+        <li>
           <Link href="/tiktok">Tiktok</Link>
         </li>
       </ul>
-
-      {/* <ul>
-        <li className={styles.item}>
-          <a href="/facebook">Facebook</a>
-        </li>
-        <li className={styles.item}>
-          <a href="/youtube">Youtube</a>
-        </li>
-        <li className={styles.item}>
-          <a href="/tiktok">Tiktok</a>
-        </li>
-      </ul> */}
     </main>
   );
 }
